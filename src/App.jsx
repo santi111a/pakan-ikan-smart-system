9250b631421b7586f093c9e364a62259a7fa0877
santi111a <dewisantika8116@gmail.com>
2026-05-10 14:02:53 +0700

    fix: memperbaiki styles yang menyebabkan blank page

--- src/App.jsx
+++ src/App.jsx
@@ -95,7 +95,7 @@ function App() {
 return (
     <div style={{ minHeight: '100vh', backgroundColor: '#0f172a', color: 'white', fontFamily: 'sans-serif' }}>
       
-      {/* HEADER TETAP MUNCUL DI ATAS */}
+{/* HEADER TETAP MUNCUL DI ATAS */}
 <div style={{ padding: '20px', borderBottom: '1px solid #1e293b', textAlign: 'center', background: '#0f172a' }}>
   <h2 style={{ color: '#38bdf8', margin: 0, fontSize: '18px', fontWeight: 'bold' }}>
     Smart Farming KSTM AL IHYA
