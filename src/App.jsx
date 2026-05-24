14877cd428bf2811df93838e1126618a35819007
santi111a <dewisantika8116@gmail.com>
2026-05-24 16:37:36 +0700

    baru lagi

--- src/App.jsx
+++ src/App.jsx
@@ -33,9 +33,10 @@ function App() {
   // --- AMBIL DATA REALTIME DARI FIREBASE ---
   useEffect(() => {
     const dbRef = ref(db, '/'); 
-    onValu(ref(db, '/kipas_status'), (snapshot) => {
-      const val = snapshot.val();
-   setData(prev => ({ ...prev, kipas_status: val }));
+    onValue(dbRef, (snapshot) => {
+      if (snapshot.exists()) {
+        const result = snapshot.val();
+        setData(prev => ({ ...prev, ...result }));
         
         if (result.jurnal_harian) {
           setListJurnal(Object.keys(result.jurnal_harian).map(key => ({ id: key, ...result.jurnal_harian[key] })).reverse());
@@ -50,7 +51,7 @@ function App() {
           setListPakan(Object.keys(result.jurnal_pakan).map(key => ({ id: key, ...result.jurnal_pakan[key] })).reverse());
         }
       }
-    );
+    });
   }, []);
 
   // --- FUNGSI SIMPAN & UPDATE ---
