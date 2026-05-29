0f1e8c979ad38a17002347924dbe56d5587f589f
santi111a <dewisantika8116@gmail.com>
2026-05-29 12:16:16 +0700

    bikin jadi apk

---
+++ public/service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'pakan-ikan-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch data dari cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
