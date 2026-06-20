3f5fd68d10ad8020a7927ce286d1613947e023b4
santi111a <dewisantika8116@gmail.com>
2026-06-20 13:26:23 +0700

    Update Supabase URL dan Key ke versi benar

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,8 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://tqf6sd3BIoxhRf7u67-1JA_lPiLm_EB'; // Wajib diapit tanda kutip
-const supabaseAnonKey = 'sb_publishable_QTf6sd3BIoxhRf7u67-1JA_lPiLm_EB'; // Wajib diapit tanda kutip
+// Ini adalah URL dasar proyek Anda
+const supabaseUrl = 'https://tqfspwtaexpxlmflaskd.supabase.co'
+// Ini adalah Publishable Key dari dashboard Anda
+const supabaseAnonKey = 'sb_publishable_QTf6sd3BIoxhRf7u67-1JA_lPiLm_EB'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
