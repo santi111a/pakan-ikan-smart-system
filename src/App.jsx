e4e23b17c36a67ae4334d9a61f827d6b5fdf5a6a
santi111a <dewisantika8116@gmail.com>
2026-06-23 05:27:51 +0700

    masih perbaikan

--- src/App.jsx
+++ src/App.jsx
@@ -54,8 +54,13 @@ function App() {
 
        <div style={{ marginBottom: '20px' }}>
         <label style={{ fontSize: '0.75rem', color: '#94a3b8', fontWeight: 'bold' }}>DURASI (DETIK)</label>
-        <input type="number" style={inputStyle} value={data.durasi} onChange={(e) => setData({...data, durasi: e.target.value})} />
-      </div>
+        <input 
+    type="number" 
+    style={{ ...inputStyle, width: '100px' }} 
+    value={data.durasi} 
+    onChange={(e) => setData({...data, durasi: e.target.value})} 
+  />
+</div>
 
           <button style={buttonStyle} onClick={() => alert("Data Tersimpan!")}>SIMPAN DATA</button>
         </div>
